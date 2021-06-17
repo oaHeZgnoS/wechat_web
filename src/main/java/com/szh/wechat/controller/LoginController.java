cee154965bef4a76513b6e12e76fcd572d5c3e96
songzehao <1434928030@qq.com>
2021-06-17 18:14:09 +0800

    添加图形验证码后端逻辑

--- src/main/java/com/szh/wechat/controller/LoginController.java
+++ src/main/java/com/szh/wechat/controller/LoginController.java
@@ -1,8 +1,14 @@
 package com.szh.wechat.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +16,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.google.code.kaptcha.Constants;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
 import com.szh.wechat.model.User;
 import com.szh.wechat.service.UserService;
 
@@ -22,6 +32,9 @@ public class LoginController {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private DefaultKaptcha defaultKaptcha;
 
 	@GetMapping("/login")
 	public String login() {
@@ -71,6 +84,52 @@ public class LoginController {
 	@GetMapping("/fail")
 	public String toFail() {
 		return "fail";
+	}
+
+	@RequestMapping(value = "/captcha")
+	public void getKaptchaImage(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// 用字节数组存储
+		byte[] captchaChallengeAsJpeg = null;
+		ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+		ServletOutputStream responseOutputStream = response.getOutputStream();
+		final HttpSession httpSession = request.getSession();
+		try {
+			// 生产验证码字符串并保存到session中
+			String createText = defaultKaptcha.createText();
+			// 打印随机生成的字母和数字
+			log.debug(createText);
+			httpSession.setAttribute(Constants.KAPTCHA_SESSION_KEY, createText);
+			// 使用生产的验证码字符串返回一个BufferedImage对象并转为byte写入到byte数组中
+			BufferedImage challenge = defaultKaptcha.createImage(createText);
+			ImageIO.write(challenge, "jpg", jpegOutputStream);
+			captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
+			response.setHeader("Cache-Control", "no-store");
+			response.setHeader("Pragma", "no-cache");
+			response.setDateHeader("Expires", 0);
+			response.setContentType("image/jpeg");
+			// 定义response输出类型为image/jpeg类型，使用response输出流输出图片的byte数组
+			responseOutputStream.write(captchaChallengeAsJpeg);
+			responseOutputStream.flush();
+		} catch (IllegalArgumentException e) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return;
+		} finally {
+			responseOutputStream.close();
+		}
+	}
+
+	@RequestMapping(value = "/checkcode", method = RequestMethod.POST, produces = "text/html; charset=utf-8")
+	public boolean checkcode(HttpServletRequest request) {
+		String captchaId = (String) request.getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY);
+		String parameter = request.getParameter("veritycode");
+		System.out.println("Session  vrifyCode " + captchaId + " form veritycode " + parameter);
+		if (!captchaId.equals(parameter)) {
+			log.debug("验证码错误");
+			return false;
+		} else {
+			log.debug("登录成功");
+			return true;
+		}
 	}
 
 }
